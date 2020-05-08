b272b57e6683c6e85e4d26e806c3eda5f1fbf720
CassielJones2 <CassielJones@protonmail.com>
2020-05-08 16:39:13 +0800

    add iconPath

--- src/main/index.js
+++ src/main/index.js
@@ -1,6 +1,7 @@
 import { app, BrowserWindow, Tray, Menu, ipcMain, nativeImage } from 'electron'
 import path from 'path'
 
+
 /**
  * Set `__static` path to static files in production
  * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-static-assets.html
@@ -9,6 +10,7 @@ if (process.env.NODE_ENV !== 'development') {
     global.__static = require('path').join(__dirname, '/static').replace(/\\/g, '\\\\')
 }
 
+let appIcon = null
 let mainWindow
 const winURL = process.env.NODE_ENV === 'development' ?
     `http://localhost:9080` :
@@ -33,6 +35,48 @@ function createWindow() {
     mainWindow.on('closed', () => {
         mainWindow = null
     })
+
+    ipcMain.on('asynchronous-show', (event, arg) => {
+        if (arg === 'show') {
+            mainWindow.minimize()
+        } else if (arg === 'max') {
+            console.log(mainWindow.isMaximized())
+            if (mainWindow.isMaximized()) {
+                mainWindow.unmaximize()
+            } else {
+                mainWindow.maximize()
+            }
+
+        } else {
+            mainWindow.hide()
+        }
+
+    })
+
+    const iconPath = path.join(__static, '/imgs/icon.png')
+    const trayUrl = nativeImage.createFromPath(iconPath)
+    appIcon = new Tray(trayUrl);
+
+    appIcon.on('click', function() {
+        mainWindow.show()
+    })
+
+    const contextMenu = Menu.buildFromTemplate([{
+            label: 'show',
+            click: function() {
+                mainWindow.show()
+            }
+        },
+        {
+            label: 'quit',
+            click: function() {
+                app.quit()
+            }
+        }
+
+    ])
+    appIcon.setToolTip('我的软件')
+    appIcon.setContextMenu(contextMenu)
 }
 
 app.on('ready', createWindow)
--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -1,14 +1,43 @@
 <template>
-  <div>
-      Hello Word
-  </div>
+  <header>
+        <div class="header">
+          <span @click="mizeShow('show')">
+              <img src="../../../static/imgs/yi.png" alt="">
+          </span>
+          <span @click="mizeShow('max')">
+              <img src="../../../static/imgs/kou.png" alt="">
+          </span>
+          <span @click="mizeShow('quit')">
+              <img src="../../../static/imgs/shanchushaixuanxiang.png" alt="">                
+          </span>
+      </div>
+  </header>
 </template>
 <script>
+import {ipcRenderer} from 'electron'
 export default {
   data () {
     return {
 
     }
+  },
+  methods:{
+        mizeShow (val) {
+          ipcRenderer.send("asynchronous-show",val)
+      },
   }
 }
 </script>
+<style lang="scss" scoped>
+    .header{
+        height: 20px;
+        background: #001529;
+        text-align: right;
+        padding: 0 20px;
+        img{
+            height: 100%;
+            -webkit-app-region: no-drag;
+            
+        }
+    }
+</style>
